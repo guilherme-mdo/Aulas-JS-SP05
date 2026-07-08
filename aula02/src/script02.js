69a693d073fc7b395566518bacf6d4e5bb419cce
Guilherme <guilhermemd011@gmail.com>
2026-07-07 22:26:40 -0300

    aula 2 de js

---
+++ aula02/src/script02.js
@@ -0,0 +1,19 @@
+// Exibindo conteúdo com innerHTML
+
+document.getElementById('ex1').innerHTML = "Hello, World!";
+
+// Exibindo conteúdo com innerText
+
+document.getElementById('ex2').innerText = "Olá, Mundo!";
+
+// Exibindo conteúdo com o document.write
+
+document.write(50 + 50);
+
+// Saída de dados com o alert()
+
+// alert('Isso é um alert');
+
+// Saída de dados com console.log()
+
+console.log('somente a dor coletiva gera união');
