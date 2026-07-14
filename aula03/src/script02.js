51f7dc36d82ea4958c6373c195baf557be250d4b
Guilherme <guilhermemd011@gmail.com>
2026-07-13 22:09:37 -0300

    Aula 03 de JS

---
+++ aula03/src/script02.js
@@ -0,0 +1,52 @@
+// Exemplos utilizando o typeof
+
+console.log(typeof '');
+console.log(typeof '56');
+console.log(typeof 'Bom dia');
+
+console.log('');
+
+console.log(typeof 0);
+console.log(typeof 27);
+console.log(typeof (4));
+
+// Exemplos de string
+
+console.log('');
+
+let primeiroNome = "Guilherme";
+let segundoNome = 'Michael';
+let nomeComposto = 'Guilherme Michael';
+
+console.log('Primeiro nome:' + ' ' + primeiroNome);
+console.log('Segundo nome: ' + segundoNome);
+console.log('Nome composto: ' + nomeComposto);
+
+// Exemplos de number
+
+console.log('');
+
+let num1 = 0;
+let num2 = 80;
+let num3 = 190;
+
+console.log('Primeiro número: ' + num1);
+console.log('Segundo número: ' + num2);
+console.log('Terceiro número: ' + num3);
+
+//  Exemplos de boolean
+
+console.log('');
+
+let valor = 5;
+
+console.log(valor > 7);
+console.log('5 é maior que 7: ' + (valor > 7));
+console.log('5 é menor que 545: ' + (5 < 545));
+
+// Exemplo de undefined
+
+console.log('');
+
+let semValor;
+console.log(semValor);
