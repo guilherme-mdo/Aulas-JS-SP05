76770a763dab28b5827fcf97d99e74273ad53bc7
Guilherme <guilhermemd011@gmail.com>
2026-07-15 22:17:03 -0300

    Conteúdo aula 04 JS

---
+++ aula04/src/script.js
@@ -0,0 +1,64 @@
+// Operador de atribuição
+
+let x = 10;
+
+console.log('A variável x recebe o valor de: ' + x);
+
+// Operador de adição
+
+let num1 = 3;
+let num2 = 4;
+let soma = num1 + num2;
+console.log('O resultado da soma de 3 com 4 é: '+ soma);
+
+// Operador de multiplicação
+
+console.log('');
+
+let mult = 5 * 5;
+
+console.log('O resultado da multiplicação de 5 por 5 é: ' + mult);
+
+// Operadores aritméticos
+
+console.log('');
+
+let a = 2;
+let calculo = (200 + 50) * a;
+
+console.log('O resultado da expressão é: ' + calculo);
+
+// Concatenando strings
+
+console.log('');
+
+let primeiroNome = "Guilherme";
+let ultimoNome = 'Oliveira';
+let nomeCompleto = primeiroNome + " " + ultimoNome;
+
+console.log('O meu nome completo é: ' + nomeCompleto);
+
+let texto1 = 'Que dia';
+texto1 += ' maravilhoso!';
+
+console.log(texto1);
+
+console.log('');
+
+let somaNumeros = 10 + 8;
+let juntaStringNumero = '10' + 10;
+let juntaStringNumer02 = 'SP0' + 5;
+
+console.log(somaNumeros);
+console.log(juntaStringNumero);
+console.log(juntaStringNumer02);
+
+// Operador de comparação
+
+console.log('');
+
+let n = 3;
+
+console.log('2 é maior que 3?: ' + (2 > n));
+
+console.log('7 é maior que 3?: ' + (7 > n));
---
+++ aula04/src/script02.js
@@ -0,0 +1,78 @@
+console.log('teste');
+
+// Opeações aritméticas
+
+let soma1 = 100 + 200;
+let a = 10;
+let b = 3;
+let soma2 = a + b;
+let expressao = (100 + 50) * a;
+
+console.log(soma1);
+console.log(soma2);
+console.log(expressao);
+
+// Operação soma
+
+console.log('');
+
+let exSoma1 =  5 + 10;
+console.log ('5 + 10 =' + exSoma1);
+
+// Operação subtração
+
+console.log('');
+
+let exSubtracao1 = 10 - 5;
+console.log("10 - 5 = " + exSubtracao1);
+
+// Operação multiplicação
+
+console.log('');
+
+let exMultiplicacao1 = 2 * 4;
+console.log('2 x 4 = ' + exMultiplicacao1);
+
+// Operação divisão
+
+console.log('');
+
+let exDivisao = 8 / 2;
+console.log('8 : 2 = ' + exDivisao);
+
+// Operação módulo
+console.log('');
+
+let exModulo = 5 % 2;
+console.log('Módulo de 5 : 2 = ' + exModulo);
+
+// incremento
+
+console.log('');
+
+let c = 0;
+c++;
+let exIncremento1 = c;
+
+console.log('Incremento de 0 = ' + exIncremento1);
+
+// decremento
+
+console.log('');
+
+let d = 5;
+d--;
+let exDecremento1 = d;
+
+console.log('Decremento de 5 = ' + exDecremento1);
+
+// Operação exponenciação
+console.log('');
+
+let exExponenciacao = 4 ** 2;
+
+console.log('4² = ' + exExponenciacao);
+
+let e = 4;
+let exExponenciacao2 = Math.pow(e, 2);
+console.log('4² = ' + exExponenciacao2);
